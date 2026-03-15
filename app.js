7913ab2ca7e6da8c74904920bb2207a718e8839b
Alberto Leon <aleogon@MacBook-Air-de-Alberto.local>
2026-03-15 12:31:57 +0100

    Creacion del servidor y primer endpoint

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("¡Hola, Backend! Este es mi primer commit.");
+const express = require('express'); // Importamos Express
+const app = express(); // Creamos la aplicación
+const port = 3000; // Definimos el puerto donde va a escuchar
+
+// Creamos nuestra primera "Ruta" (Endpoint)
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo! Mi servidor Express está vivo.');
+});
+
+// Encendemos el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
